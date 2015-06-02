0ed68fa788b3537170a56ab6d70eb833196f42db
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2015-06-02 10:53:39 +0200

    Nix expression to right setup testing

---
+++ default.nix
@@ -0,0 +1,22 @@
+let
+    pkgs = import <nixpkgs> {};
+  in
+  {  stdenv ? pkgs.stdenv
+    ,python3 ? pkgs.python3
+  }:
+
+  stdenv.mkDerivation {
+    name = "python-nix";
+    version = "0.0.0.0";
+    src = ./.;
+    buildInputs = with pkgs; [
+      python3
+      python34Packages.pygit2
+      python34Packages.sqlalchemy9
+      python34Packages.psycopg2
+      python34Packages.requests
+    ];
+    shellHook = ''
+      echo -e "\nLoading nix-shell with python3 deps..."
+    '';
+  }
