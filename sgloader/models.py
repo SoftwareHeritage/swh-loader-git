807895956ef30b2a15a478377075e18c3c3bfcc6
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2015-06-03 13:00:33 +0200

    Add AUTHORS file at top-level

--- sgloader/models.py
+++ sgloader/models.py
@@ -1,4 +1,5 @@
 # Copyright (C) 2015  Stefano Zacchiroli <zack@upsilon.cc>, Antoine R. Dumont <antoine.romain.dumont@gmail.com>
+# See the AUTHORS file at the top-level directory of this distribution
 # License: GNU General Public License version 3, or any later version
 # See top-level LICENSE file for more information
 
