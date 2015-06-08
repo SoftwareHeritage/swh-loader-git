513dc8d3b94c2b00ced2226c6cb2100cba5a3cba
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2015-06-08 14:28:33 +0200

    Update default.nix expression

--- default.nix
+++ default.nix
@@ -11,8 +11,8 @@ let
     src = ./.;
     buildInputs = with pkgs; [
       python3
+      python34Packages.ipython
       python34Packages.pygit2
-      python34Packages.sqlalchemy9
       python34Packages.psycopg2
       python34Packages.requests
       python34Packages.nose
