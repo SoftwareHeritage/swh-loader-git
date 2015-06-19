0ed2e55adf24521a57c2b63105d755f140258b7a
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2015-06-19 15:13:38 +0200

    Refactor - Reuse already computed information

--- swh/storage/store.py
+++ swh/storage/store.py
@@ -39,14 +39,13 @@ def find(config, git_object):
         return _find_fn[type](db_conn, sha1_bin, type)
 
 
-def _add_blob(db_conn, config, git_object):
+def _add_blob(db_conn, config, git_object, sha1_bin):
     """Add a blob to storage.
     Designed to be wrapped in a db transaction.
     Returns:
     - True if everything went alright.
     - None if the git sha1 was not rightly formatted.
     """
-    sha1_bin = hex_to_bin(git_object['sha1'])
     obj_git_sha1 = git_object['git-sha1']
     obj_git_sha_bin = hex_to_bin(obj_git_sha1)
     if obj_git_sha_bin is None:
@@ -60,20 +59,16 @@ def _add_blob(db_conn, config, git_object):
     return True
 
 
-def _add_object(db_conn, config, git_object):
+def _add_object(db_conn, config, git_object, sha1_bin):
     """Add a commit/tree to storage.
     Designed to be wrapped in a db transaction.
     Returns:
     - True if everything went alright.
     """
-    sha1_hex = git_object['sha1']
-    sha1_bin = hex_to_bin(sha1_hex)
-    content = git_object['content']
-
     folder_depth = config['folder_depth']
     fs.write_object(config['object_content_storage_dir'],
-                    sha1_hex,
-                    content,
+                    git_object['sha1'],
+                    git_object['content'],
                     folder_depth)
     models.add_object(db_conn, sha1_bin, git_object['type'])
     return True
@@ -93,7 +88,7 @@ def add(config, git_object):
 
     with db.connect(config['db_url']) as db_conn:
         try:
-            return _store_fn[type](db_conn, config, git_object)
+            return _store_fn[type](db_conn, config, git_object, sha1_bin)
         except IOError:
             db_conn.rollback()
             return False
