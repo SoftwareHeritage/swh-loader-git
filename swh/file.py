7ff924d490e2630595110742c287f9278306017a
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2015-06-22 13:07:56 +0200

    Refactor - Add retry policy on flushing to disk

--- swh/file.py
+++ swh/file.py
@@ -7,19 +7,38 @@
 import os
 import gzip
 
+from retrying import retry
+
 
 def folder_path(prefix_dir, hexhash, depth=4):
     """Compute the folder prefix from a hexhash key.
-    The depth determines the number of subfolder from prefix_dir.
-    Default to 4.
+       The depth determines the number of subfolder from prefix_dir.
+       Default to 4.
+       Example:
+       - prefix_dir: /some/path
+       - depth:      2
+       - hash:       aabbccddeeffgghhii
+       -> folder:    /some/path/aa/bb/
     """
     hexhashes = [hexhash[x:x+2] for x in range(0, 2*depth, 2)]
     return os.path.join(prefix_dir, *hexhashes)
 
 
+def _retry_if_io_error(exc):
+    """Return True if we should retry (in this case when it's an IOError),
+       False otherwise.
+    """
+    return isinstance(exc, IOError)
+
+
+@retry(retry_on_exception=_retry_if_io_error, wrap_exception=True)
 def write_data(data, path, comp_flag=None):
     """Write data to path.
-    If compress_path is not None, gzip the data.
+       If compress_path is not None, gzip the data.
+
+       If an IOError is raised, this function will be triggered immediately
+       again.
+       Otherwise, if any other error is raised, the error will be wrapped in RetryError.
     """
     with (gzip.open(path, 'wb') if comp_flag else open(path, 'wb')) as f:
         return f.write(bytes(data, 'utf-8'))
