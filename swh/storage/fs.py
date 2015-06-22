79bdda5a08b38d9197e02d6fb316af4dffc3b62d
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2015-06-22 13:23:11 +0200

    Improve docstring

--- swh/storage/fs.py
+++ swh/storage/fs.py
@@ -13,6 +13,9 @@ from swh import file
 
 def create_dir_from_hash(storage_dir, hashv, folder_depth):
     """Create directory in folder storage_dir from a given hash value.
+       - storage_dir: prefix folder path to store data
+       - hashv:       hash value to use as suffix-folder
+       - depth:       depth to extract suffix from hashv
     """
     folder_in_storage = file.folder_path(storage_dir, hashv,
                                          folder_depth)
@@ -22,6 +25,11 @@ def create_dir_from_hash(storage_dir, hashv, folder_depth):
 
 def write_object(dataset_dir, hashv, data, folder_depth, compress_flag=None):
     """Write object with data and hashv on disk in dataset_dir.
+       - storage_dir:   prefix folder path to store data
+       - hashv:         hash value to use as suffix-folder
+       - data:          data to store on disk
+       - depth:         depth to extract suffix from hashv
+       - compress_flag: is the data to be compressed (gzipped) or not
     """
     folder_in_storage = create_dir_from_hash(dataset_dir, hashv, folder_depth)
     filepath = os.path.join(folder_in_storage, hashv)
