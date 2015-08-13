789a580325d09553fd9d8ae01bfe2e0b14001f1f
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2015-08-13 18:38:42 +0200

    Refactor - format pep8

--- swh/gitloader/store.py
+++ swh/gitloader/store.py
@@ -8,31 +8,28 @@ from swh.storage import store
 from swh.http import client
 
 
-def store_objects(backend_url, obj_type, swhmap):
+def store_objects(back_url, obj_type, swhmap):
     """Load objects to the backend.
     """
     sha1s = swhmap.keys()
     # have: filter unknown obj
-    unknown_obj_sha1s = client.post(backend_url,
-                                    obj_type,
-                                    sha1s)
-
+    unknown_obj_sha1s = client.post(back_url, obj_type, sha1s)
     if not unknown_obj_sha1s:
         return True
 
     # seen: now create the data for the backend to store
     obj_map = swhmap.objects()
     # store unknown objects
-    return client.put(backend_url, obj_type, map(obj_map.get, unknown_obj_sha1s))
+    return client.put(back_url, obj_type, map(obj_map.get, unknown_obj_sha1s))
 
 
-def load_to_back(backend_url, swhrepo):
-    """Load to the backend_url the repository swhrepo.
+def load_to_back(back_url, swhrepo):
+    """Load to the back_url the repository swhrepo.
     """
     # First, store/retrieve the origin identifier
-    # FIXME: should be done by the cloner worker (which is not yet plugged on the
-    # right swh db ftm)
-    client.put(backend_url,
+    # FIXME: should be done by the cloner worker (which is not yet plugged on
+    # the right swh db ftm)
+    client.put(back_url,
                obj_type=store.Type.origin,
                obj=swhrepo.get_origin())
 
@@ -41,20 +38,22 @@ def load_to_back(backend_url, swhrepo):
     # - then the backend answers the checksums it does not know
     # - then the worker sends only what the backend does not know per
     # object type basis
-    store_objects(backend_url, store.Type.content, swhrepo.get_contents())
+    store_objects(back_url, store.Type.content, swhrepo.get_contents())
     # the contents could fail, still we can continue with directories
 
-    res = store_objects(backend_url, store.Type.directory, swhrepo.get_directories())
+    res = store_objects(back_url, store.Type.directory,
+                        swhrepo.get_directories())
     if res:
-        res = store_objects(backend_url, store.Type.revision, swhrepo.get_revisions())
+        res = store_objects(back_url, store.Type.revision,
+                            swhrepo.get_revisions())
         if res:
             # brutally send all remaining occurrences
-            client.put(backend_url,
+            client.put(back_url,
                        store.Type.occurrence,
                        swhrepo.get_occurrences())
 
-            # and releases (the idea here is that compared to existing other objects,
-            # the quantity is less)
-            client.put(backend_url,
+            # and releases (the idea here is that compared to existing other
+            # objects, the quantity is less)
+            client.put(back_url,
                        store.Type.release,
                        swhrepo.get_releases())
