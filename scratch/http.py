408a1a5c8a62ab9832a66be03e7f3a593d23fd22
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2015-09-02 17:49:13 +0200

    Clean scratch folder

--- scratch/http.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-
-import requests
-
-response = requests.get('http://localhost:5000/commits/1')
-
-print(response)
--- scratch/scratch-swh-api.py
+++
@@ -1,5 +0,0 @@
-from swh.storage import store
-from swh.protocols import serial
-from swh.client import http
-
-r = http.put('http://localhost:5000', store.Type.origin, {'url': 'https://github.com/swh/good.git', 'type': git})
