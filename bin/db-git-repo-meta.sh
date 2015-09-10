b7729b9c37096ee38fb9a2c34416e7702eaa42cf
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2015-09-10 10:46:28 +0200

    Update the db metadata script extractor

--- bin/db-git-repo-meta.sh
+++ bin/db-git-repo-meta.sh
@@ -13,16 +13,18 @@ count() {
 
 NB_CONTENTS=$(count $DB "select count(*) from content;")
 NB_DIRECTORIES=$(count $DB "select count(*) from directory;")
-NB_DIRECTORY_ENTRIES=$(count $DB "select count(*) from directory_entry;")
+NB_DIRECTORY_ENTRIES_DIR=$(count $DB "select count(*) from directory_entry_dir;")
+NB_DIRECTORY_ENTRIES_FILE=$(count $DB "select count(*) from directory_entry_file;")
 NB_REVISIONS=$(count $DB "select count(*) from revision;")
 NB_RELEASES=$(count $DB "select count(*) from release;")
 NB_PERSONS=$(count $DB "select count(*) from person;")
 
 cat<<EOF
-content           $NB_CONTENTS
-directory         $NB_DIRECTORIES
-directory_entries $NB_DIRECTORY_ENTRIES
-revision          $NB_REVISIONS
-release           $NB_RELEASES
-person            $NB_PERSONS
+content                $NB_CONTENTS
+directory              $NB_DIRECTORIES
+directory_entries_dir  $NB_DIRECTORY_ENTRIES_DIR
+directory_entries_file $NB_DIRECTORY_ENTRIES_FILE
+revision               $NB_REVISIONS
+release                $NB_RELEASES
+person                 $NB_PERSONS
 EOF
