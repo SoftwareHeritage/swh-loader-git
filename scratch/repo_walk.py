4d87a865d85b8673314723f8c0dc499d256c1706
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2015-09-14 12:25:53 +0200

    Update: repo walk and check the submodules
    's behavior.
    Submodules are not found in the pygit2's repo object model

--- scratch/repo_walk.py
+++ scratch/repo_walk.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 
+
 import pygit2
-from pygit2 import GIT_SORT_TOPOLOGICAL
+from pygit2 import GIT_OBJ_COMMIT, GIT_OBJ_TREE, GIT_SORT_TOPOLOGICAL
 
 import sys
+
 
 repo_path = sys.argv[1]
 ref_name = sys.argv[2]
@@ -15,6 +17,13 @@ ref = repo.lookup_reference(ref_name)
 head_rev = repo[ref.target]
 
 for rev in repo.walk(head_rev.hex, GIT_SORT_TOPOLOGICAL):
-    print(rev.hex, rev.tree.hex)
+    print("revision: %s, revision.tree: %s" % (rev.hex, rev.tree.hex))
     for tree_entry in rev.tree:
-        print(repo.get(tree_entry.oid))
+        obj = repo.get(tree_entry.oid)
+        print("tree_entry: %s, obj: %s" % (tree_entry, obj))
+        if obj is None:  #.type == GIT_OBJ_COMMIT: -> submodule are not in repo object
+            print('submodule revision: %s' % tree_entry.hex)
+        elif obj.type == GIT_OBJ_TREE:
+            print('tree')
+        else:
+            print('blob')
