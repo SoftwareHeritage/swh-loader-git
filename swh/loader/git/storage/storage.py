9fd9fd55cc4412d01a0a70874db49406181abef0
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2015-09-15 20:06:04 +0200

    Update local loader to use byte sha1s

--- swh/loader/git/storage/storage.py
+++ swh/loader/git/storage/storage.py
@@ -6,7 +6,7 @@
 from io import StringIO
 
 from swh.loader.git.storage import models
-
+from swh.core import hashutil
 
 Type = models.Type
 
@@ -15,9 +15,10 @@ _find_object = {Type.occurrence: models.find_occurrences_for_revision,
                 Type.content: lambda *args: models.find_object(*args,
                                                                column='sha1')}
 
+hex_to_sha1 = lambda x: '\\\\x%s' % hashutil.hash_to_hex(x)
 
 def find(db_conn, id, type):
-    """Find an object according to its sha1hex and type.
+    """Find an object according to its id and type.
     """
     return _find_object.get(type, models.find_object)(db_conn, id, type)
 
@@ -33,20 +34,21 @@ def find_unknowns(db_conn, obj_type, sha1s_hex):
     def row_to_sha1(row):
         """Convert a row (memoryview) to a string sha1.
         """
-        return row[0]
+        return row[0].tobytes()
 
-    vals = '\n'.join(sha1s_hex)
     cpy_data_buffer = StringIO()
+    vals = '\n'.join(map(hex_to_sha1,
+                         sha1s_hex))
     cpy_data_buffer.write(vals)
     cpy_data_buffer.seek(0)  # move file cursor back at start of file
 
     find_unknown_fn = _find_unknown[obj_type]
     unknowns = find_unknown_fn(db_conn, cpy_data_buffer)
     cpy_data_buffer.close()
-    return list(map(row_to_sha1, unknowns))
+    return map(row_to_sha1, unknowns)
 
 
-def _add_content(db_conn, vcs_object, sha1hex):
+def _add_content(db_conn, vcs_object, id):
     """Add a blob to storage.
     Designed to be wrapped in a db transaction.
     Returns:
@@ -56,25 +58,25 @@ def _add_content(db_conn, vcs_object, sha1hex):
     caller.
     """
     models.add_content(db_conn,
-                       sha1hex,
+                       id,
                        vcs_object['git-sha1'],
                        vcs_object['content-sha256'],
                        vcs_object['size'])
-    return sha1hex
+    return id
 
 
-def _add_directory(db_conn, vcs_object, sha1hex):
+def _add_directory(db_conn, vcs_object, id):
     """Add a directory to storage.
     Designed to be wrapped in a db transaction.
     """
-    parent_id = models.add_directory(db_conn, sha1hex)
+    parent_id = models.add_directory(db_conn, id)
     for directory_entry_dir in vcs_object['entry-dirs']:
         _add_directory_entry_dir(db_conn, parent_id, directory_entry_dir)
     for directory_entry_file in vcs_object['entry-files']:
         _add_directory_entry_file(db_conn, parent_id, directory_entry_file)
     for directory_entry_rev in vcs_object['entry-revs']:
         _add_directory_entry_rev(db_conn, parent_id, directory_entry_rev)
-    return sha1hex
+    return id
 
 
 def _add_directory_entry_dir(db_conn, parent_id, vcs_object):
@@ -140,7 +142,7 @@ def _add_directory_entry_rev(db_conn, parent_id, vcs_object):
     return parent_id
 
 
-def _add_revision(db_conn, vcs_object, sha1hex):
+def _add_revision(db_conn, vcs_object, id):
     """Add a revision to storage.
     Designed to be wrapped in a db transaction.
     Returns:
@@ -150,7 +152,7 @@ def _add_revision(db_conn, vcs_object, sha1hex):
     caller.
     """
     models.add_revision(db_conn,
-                        sha1hex,
+                        id,
                         vcs_object['date'],
                         vcs_object['committer-date'],
                         vcs_object['directory'],
@@ -158,23 +160,23 @@ def _add_revision(db_conn, vcs_object, sha1hex):
                         vcs_object['author'],
                         vcs_object['committer'],
                         vcs_object['parent-sha1s'])
-    return sha1hex
+    return id
 
 
-def _add_release(db_conn, vcs_object, sha1hex):
+def _add_release(db_conn, vcs_object, id):
     """Add a release.
     """
     models.add_release(db_conn,
-                       sha1hex,
+                       id,
                        vcs_object['revision'],
                        vcs_object['date'],
                        vcs_object['name'],
                        vcs_object['comment'],
                        vcs_object['author'])
-    return sha1hex
+    return id
 
 
-def _add_occurrence(db_conn, vcs_object, sha1hex):
+def _add_occurrence(db_conn, vcs_object, id):
     """Add an occurrence.
     """
     models.add_occurrence_history(db_conn,
@@ -182,7 +184,7 @@ def _add_occurrence(db_conn, vcs_object, sha1hex):
                                   vcs_object['branch'],
                                   vcs_object['revision'],
                                   vcs_object['authority'])
-    return sha1hex
+    return id
 
 
 def add_person(db_conn, vcs_object):
@@ -229,7 +231,7 @@ def add_with_fs_storage(db_conn, config, id, type, vcs_object):
 
 
 def add(db_conn, config, id, type, vcs_object):
-    """Given a sha1hex, type and content, store a given object in the store.
+    """Given a id, type and content, store a given object in the store.
     - db_conn is the opened connection to the db
     - config is not used here
     - type is the object's type
@@ -237,9 +239,13 @@ def add(db_conn, config, id, type, vcs_object):
     """
     return _store_fn[type](db_conn, vcs_object, id)
 
+hex_to_sha1_2 = lambda x: '\\x%s' % hashutil.hash_to_hex(x)
 
 def add_revision_history(db_conn, tuple_parents):
     """Given a list of tuple (sha, parent_sha), store in revision_history.
     """
     if len(tuple_parents) > 0:
-        models.add_revision_history(db_conn, tuple_parents)
+        models.add_revision_history(
+            db_conn,
+            map(lambda t: (hex_to_sha1_2(t[0]), hex_to_sha1_2(t[1]), t[2]),
+                tuple_parents))
