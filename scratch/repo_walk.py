42b9418a801e7c9f6dc4c441803c24579a1eb0b8
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2015-09-15 15:33:19 +0200

    Improve repo_walk sample

--- scratch/repo_walk.py
+++ scratch/repo_walk.py
@@ -8,22 +8,20 @@ import sys
 
 
 repo_path = sys.argv[1]
-ref_name = sys.argv[2]
 
 repo = pygit2.Repository(repo_path)
 
-ref = repo.lookup_reference(ref_name)
+for ref_name in repo.listall_references():
+    print(ref_name)
+    ref = repo.lookup_reference(ref_name)
+    head_rev = repo[ref.target]  # noqa
 
-head_rev = repo[ref.target]
-
-for rev in repo.walk(head_rev.hex, GIT_SORT_TOPOLOGICAL):
-    print("revision: %s, revision.tree: %s" % (rev.hex, rev.tree.hex))
-    for tree_entry in rev.tree:
-        obj = repo.get(tree_entry.oid)
-        print("tree_entry: %s, obj: %s" % (tree_entry, obj))
-        if obj is None:  #.type == GIT_OBJ_COMMIT: -> submodule are not in repo object
-            print('submodule revision: %s' % tree_entry.hex)
-        elif obj.type == GIT_OBJ_TREE:
-            print('tree')
-        else:
-            print('blob')
+    for rev in repo.walk(head_rev.hex, GIT_SORT_TOPOLOGICAL):
+        for tree_entry in rev.tree:
+            obj = repo.get(tree_entry.oid)
+            if obj is None:  #.type == GIT_OBJ_COMMIT: -> submodule are not in repo object
+                print('subr: %s' % tree_entry.hex)
+            elif obj.type == GIT_OBJ_TREE:
+                print('tree: %s' % tree_entry.hex)
+            else:
+                print('blob: %s' % tree_entry.hex)
