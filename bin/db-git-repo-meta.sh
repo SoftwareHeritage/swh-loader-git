d0c06b7fb93fa35824e26e1829f4af5ab8e78c9b
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2015-09-15 15:56:43 +0200

    Update script to check db metadata after loading

--- bin/db-git-repo-meta.sh
+++ bin/db-git-repo-meta.sh
@@ -15,6 +15,7 @@ NB_CONTENTS=$(count $DB "select count(*) from content;")
 NB_DIRECTORIES=$(count $DB "select count(*) from directory;")
 NB_DIRECTORY_ENTRIES_DIR=$(count $DB "select count(*) from directory_entry_dir;")
 NB_DIRECTORY_ENTRIES_FILE=$(count $DB "select count(*) from directory_entry_file;")
+NB_DIRECTORY_ENTRIES_REV=$(count $DB "select count(*) from directory_entry_rev;")
 NB_REVISIONS=$(count $DB "select count(*) from revision;")
 NB_RELEASES=$(count $DB "select count(*) from release;")
 NB_PERSONS=$(count $DB "select count(*) from person;")
@@ -24,6 +25,7 @@ content                $NB_CONTENTS
 directory              $NB_DIRECTORIES
 directory_entries_dir  $NB_DIRECTORY_ENTRIES_DIR
 directory_entries_file $NB_DIRECTORY_ENTRIES_FILE
+directory_entries_rev  $NB_DIRECTORY_ENTRIES_REV
 revision               $NB_REVISIONS
 release                $NB_RELEASES
 person                 $NB_PERSONS
