b3479f5b58ec4a7ae318cd75bb49c7170b876dfc
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2016-10-26 11:00:58 +0200

    git-remote-reader: Add the missing return statement

--- swh/loader/git/tasks.py
+++ swh/loader/git/tasks.py
@@ -50,4 +50,4 @@ class ReaderGitRepository(Task):
         loader = GitSha1RemoteReader()
         loader.log = self.log
 
-        loader.load(repo_url, base_url)
+        return loader.load(repo_url, base_url)
