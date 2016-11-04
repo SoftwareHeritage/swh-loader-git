7c4a465dab05f279eeabbdd32b02bdf843fb5acb
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2016-11-04 10:31:56 +0100

    Permit reader to only dump on stdout

--- swh/loader/git/reader.py
+++ swh/loader/git/reader.py
@@ -60,7 +60,8 @@ class DummyGraphWalker(object):
 
 
 class GitSha1RemoteReader(BulkUpdater):
-    """Disk git sha1 reader to dump only repo's content sha1 list.
+    """Read sha1 git from a remote repository and dump only repository's
+       content sha1 as list.
 
     """
     CONFIG_BASE_FILENAME = 'loader/git-remote-reader'
@@ -81,7 +82,7 @@ class GitSha1RemoteReader(BulkUpdater):
         super().__init__(SWHRepoFullRepresentation)
         self.next_task = self.config['next_task']
         self.batch_size = self.next_task['batch_size']
-        self.task_destination = self.next_task.get('queue')
+        self.task_destination = self.next_task['queue']
         self.destination = self.next_task['destination']
 
     def graph_walker(self):
@@ -132,9 +133,7 @@ class GitSha1RemoteReader(BulkUpdater):
            contents' sha1.
 
         Returns:
-            If the configuration holds a destination queue, send those
-            sha1s as batch of sha1s to it for consumption.  Otherwise,
-            returns the list of discovered sha1s.
+            Returns the list of discovered sha1s for that origin.
 
         """
         try:
@@ -144,10 +143,21 @@ class GitSha1RemoteReader(BulkUpdater):
             return []
 
         self.fetch_data()
-        data = self.type_to_ids[b'blob']
+        return self.type_to_ids[b'blob']
 
-        if not self.task_destination:  # to stdout
-            return data
+
+class GitSha1RemoteReaderAndSendToQueue(GitSha1RemoteReader):
+    """Read sha1 git from a remote repository and dump only repository's
+       content sha1 as list and send batch of those sha1s to a celery
+       queue for consumption.
+
+    """
+    def load(self, *args, **kwargs):
+        """Retrieve the list of sha1s for a particular origin and send those
+           sha1s as group of sha1s to a specific queue.
+
+        """
+        data = super().load(*args, **kwargs)
 
         from swh.scheduler.celery_backend.config import app
         try:
@@ -169,21 +179,25 @@ class GitSha1RemoteReader(BulkUpdater):
 
 @click.command()
 @click.option('--origin-url', help='Origin\'s url')
-def main(origin_url):
+@click.option('--send/--nosend', default=False, help='Origin\'s url')
+def main(origin_url, send):
     logging.basicConfig(
         level=logging.DEBUG,
         format='%(asctime)s %(process)d %(message)s'
     )
 
+    if send:
+        loader = GitSha1RemoteReaderAndSendToQueue()
+        ids = loader.load(origin_url)
+        print('%s sha1s were sent to queue' % len(ids))
+        return
+
     loader = GitSha1RemoteReader()
     ids = loader.load(origin_url)
 
     if ids:
-        count = 0
         for oid in ids:
-            print(oid)
-            count += 1
-        print("sha1s: %s" % count)
+            print(hashutil.hash_to_hex(oid))
 
 
 if __name__ == '__main__':
