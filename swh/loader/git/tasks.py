65f740c8a13cf6b986a3e68bac45a2f8b509b471
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2018-12-17 10:19:38 +0100

    loader.git.reader: Remove unused code
    The loader git reader was a tryout to make the archiver go faster. As
    it's unused and not tested, better make it go away.
    
    Related T1219

--- swh/loader/git/tasks.py
+++ swh/loader/git/tasks.py
@@ -9,7 +9,6 @@ from swh.scheduler.task import Task
 
 from .loader import GitLoader, GitLoaderFromArchive
 from .updater import BulkUpdater
-from .reader import GitSha1RemoteReaderAndSendToQueue
 
 
 # TODO: rename to LoadRemoteGitRepository
@@ -54,17 +53,3 @@ class UncompressAndLoadDiskGitRepository(Task):
 
         return loader.load(
             origin_url, archive_path, dateutil.parser.parse(date))
-
-
-class ReaderGitRepository(Task):
-    task_queue = 'swh_reader_git'
-
-    def run_task(self, repo_url, base_url=None):
-        """Read a git repository from a remote location and send sha1 to
-        archival.
-
-        """
-        loader = GitSha1RemoteReaderAndSendToQueue()
-        loader.log = self.log
-
-        return loader.load(repo_url)
