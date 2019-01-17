c7f2cb11cd9aa82c8f45f5034a31c8711023cbf6
David Douard <david.douard@sdfa3.org>
2019-01-17 11:30:58 +0100

    Rewrite celery tasks as a decorated function
    as well as the tests of the task. Requires swh-scheduler >= 0.0.39

--- swh/loader/git/tests/test_tasks.py
+++ swh/loader/git/tests/test_tasks.py
@@ -4,68 +4,51 @@
 # See top-level LICENSE file for more information
 
 import datetime
-import unittest
 from unittest.mock import patch
 
-from swh.loader.git.tasks import (
-    UpdateGitRepository, LoadDiskGitRepository,
-    UncompressAndLoadDiskGitRepository
-)
+
+@patch('swh.loader.git.loader.GitLoader.load')
+def test_git_loader(mock_loader, swh_app, celery_session_worker):
+    mock_loader.return_value = {'status': 'eventful'}
+
+    res = swh_app.send_task(
+        'swh.loader.git.tasks.UpdateGitRepository',
+        ('origin_url',))
+    assert res
+    res.wait()
+    assert res.successful()
+
+    assert res.result == {'status': 'eventful'}
+    mock_loader.assert_called_once_with('origin_url', base_url=None)
 
 
-class TestTasks(unittest.TestCase):
-    def test_check_task_name(self):
-        task = UpdateGitRepository()
-        self.assertEqual(task.task_queue, 'swh_loader_git')
+@patch('swh.loader.git.from_disk.GitLoaderFromDisk.load')
+def test_git_loader_from_disk(mock_loader, swh_app, celery_session_worker):
+    mock_loader.return_value = {'status': 'uneventful'}
 
-    @patch('swh.loader.git.loader.GitLoader.load')
-    def test_task(self, mock_loader):
-        mock_loader.return_value = {'status': 'eventful'}
-        task = UpdateGitRepository()
+    res = swh_app.send_task(
+        'swh.loader.git.tasks.LoadDiskGitRepository',
+        ('origin_url2', '/some/repo', '2018-12-10 00:00'))
+    assert res
+    res.wait()
+    assert res.successful()
 
-        # given
-        actual_result = task.run_task('origin_url')
-
-        self.assertEqual(actual_result, {'status': 'eventful'})
-
-        mock_loader.assert_called_once_with('origin_url', base_url=None)
+    assert res.result == {'status': 'uneventful'}
+    mock_loader.assert_called_once_with(
+        'origin_url2', '/some/repo', datetime.datetime(2018, 12, 10, 0, 0))
 
 
-class TestTasks2(unittest.TestCase):
-    def test_check_task_name(self):
-        task = LoadDiskGitRepository()
-        self.assertEqual(task.task_queue, 'swh_loader_git_express')
+@patch('swh.loader.git.from_disk.GitLoaderFromArchive.load')
+def test_git_loader_from_archive(mock_loader, swh_app, celery_session_worker):
+    mock_loader.return_value = {'status': 'failed'}
 
-    @patch('swh.loader.git.from_disk.GitLoaderFromDisk.load')
-    def test_task(self, mock_loader):
-        mock_loader.return_value = {'status': 'uneventful'}
-        task = LoadDiskGitRepository()
+    res = swh_app.send_task(
+        'swh.loader.git.tasks.UncompressAndLoadDiskGitRepository',
+        ('origin_url3', '/some/repo', '2017-01-10 00:00'))
+    assert res
+    res.wait()
+    assert res.successful()
 
-        # given
-        actual_result = task.run_task('origin_url2', '/some/repo',
-                                      '2018-12-10 00:00')
-
-        self.assertEqual(actual_result, {'status': 'uneventful'})
-
-        mock_loader.assert_called_once_with(
-            'origin_url2', '/some/repo', datetime.datetime(2018, 12, 10, 0, 0))
-
-
-class TestTasks3(unittest.TestCase):
-    def test_check_task_name(self):
-        task = UncompressAndLoadDiskGitRepository()
-        self.assertEqual(task.task_queue, 'swh_loader_git_archive')
-
-    @patch('swh.loader.git.from_disk.GitLoaderFromArchive.load')
-    def test_task(self, mock_loader):
-        mock_loader.return_value = {'status': 'failed'}
-        task = UncompressAndLoadDiskGitRepository()
-
-        # given
-        actual_result = task.run_task('origin_url3', '/path/repo',
-                                      '2017-01-10 00:00')
-
-        self.assertEqual(actual_result, {'status': 'failed'})
-
-        mock_loader.assert_called_once_with(
-            'origin_url3', '/path/repo', datetime.datetime(2017, 1, 10, 0, 0))
+    assert res.result == {'status': 'failed'}
+    mock_loader.assert_called_once_with(
+        'origin_url3', '/some/repo', datetime.datetime(2017, 1, 10, 0, 0))
