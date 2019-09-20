4086cab9fe6a0cbe607ab94ddab431dc24800a19
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2019-09-20 11:55:08 +0200

    package.pypi: Add tests around download function

--- swh/loader/package/tests/test_pypi.py
+++ swh/loader/package/tests/test_pypi.py
@@ -10,7 +10,9 @@ import pytest
 from os import path
 
 from swh.core.tarball import uncompress
-from swh.loader.package.pypi import PyPILoader, PyPIClient, author, sdist_parse
+from swh.loader.package.pypi import (
+    PyPILoader, PyPIClient, author, sdist_parse, download
+)
 
 
 def test_author_basic():
@@ -207,7 +209,8 @@ def test_sdist_parse(tmp_path):
     assert actual_sdist == expected_sdist
 
 
-def test_sdist_parse_ko(tmp_path):
+@pytest.mark.fs
+def test_sdist_parse_failures(tmp_path):
     """Parsing inexistant path/archive/PKG-INFO yield None"""
     # inexistant first level path
     assert sdist_parse('/something-inexistant') is None
@@ -217,6 +220,71 @@ def test_sdist_parse_ko(tmp_path):
     existing_path_no_pkginfo = str(tmp_path / 'something')
     os.mkdir(existing_path_no_pkginfo)
     assert sdist_parse(tmp_path) is None
+
+
+@pytest.mark.fs
+def test_download_fail_to_download(tmp_path, requests_mock):
+    url = 'https://pypi.org/pypi/arrow/json'
+    status_code = 404
+    requests_mock.get(url, status_code=status_code)
+
+    with pytest.raises(ValueError) as e:
+        download(url, tmp_path)
+
+    assert e.value.args[0] == "Fail to query '%s'. Reason: %s" % (
+        url, status_code)
+
+
+@pytest.mark.fs
+def test_download_fail_length_mismatch(tmp_path, requests_mock):
+    """Mismatch length after download should raise
+
+    """
+    filename = 'requests-0.0.1.tar.gz'
+    url = 'https://pypi.org/pypi/requests/%s' % filename
+    data = 'this is something'
+    wrong_size = len(data) - 3
+    requests_mock.get(url, text=data, headers={
+        'content-length': str(wrong_size)  # wrong size!
+    })
+
+    with pytest.raises(ValueError) as e:
+        download(url, dest=str(tmp_path))
+
+    assert e.value.args[0] == "Error when checking size: %s != %s" % (
+        wrong_size, len(data)
+    )
+
+
+@pytest.mark.fs
+def test_download_ok(tmp_path, requests_mock):
+    """Download without issue should provide filename and hashes"""
+    filename = 'requests-0.0.1.tar.gz'
+    url = 'https://pypi.org/pypi/requests/%s' % filename
+    data = 'this is something'
+    requests_mock.get(url, text=data, headers={
+        'content-length': str(len(data))
+    })
+
+    actual_filepath, actual_hashes = download(url, dest=str(tmp_path))
+
+    actual_filename = os.path.basename(actual_filepath)
+    assert actual_filename == filename
+    assert actual_hashes['length'] == len(data)
+    assert actual_hashes['sha1'] == 'fdd1ce606a904b08c816ba84f3125f2af44d92b2'
+    assert (actual_hashes['sha256'] ==
+            '1d9224378d77925d612c9f926eb9fb92850e6551def8328011b6a972323298d5')
+
+
+@pytest.mark.fs
+def test_download_fail_hashes_mismatch(tmp_path, requests_mock):
+    """Mismatch hash after download should raise
+
+    """
+    pass
+
+
+# LOADER SCENARIO #
 
 # "edge" cases (for the same origin) #
 
