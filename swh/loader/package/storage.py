9e3c0dc0e5f69a6ebd986038513fe24baa0f3d63
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2019-09-26 16:06:57 +0200

    package.storage: Avoid redefining passthrough methods

--- swh/loader/package/storage.py
+++ swh/loader/package/storage.py
@@ -15,11 +15,6 @@ class ProxyStorage:
         self.directories_seen = set()
         self.revisions_seen = set()
 
-    def origin_add(self, origins):
-        return self.storage.origin_add(origins)
-
-    def origin_visit_add(self, origin, date, type=None):
-        return self.storage.origin_visit_add(origin, date, type=type)
 
     def _filter_missing_contents(
             self, content_hashes: Sequence[bytes]) -> Sequence[bytes]:
@@ -100,34 +95,5 @@ class ProxyStorage:
             r for r in revisions if r['id'] in missing_ids
         )
 
-    def snapshot_add(self, snapshots):
-        return self.storage.snapshot_add(snapshots)
-
-    def origin_visit_update(self, origin, visit_id, status=None,
-                            metadata=None, snapshot=None):
-        return self.storage.origin_visit_update(
-            origin, visit_id, status=status,
-            metadata=metadata, snapshot=snapshot
-        )
-
-    def stat_counters(self):
-        return self.storage.stat_counters()
-
-    def origin_visit_get(self, origin, last_visit=None, limit=None):
-        return self.storage.origin_visit_get(
-            origin, last_visit=last_visit, limit=limit)
-
-    def content_missing_per_sha1(self, contents):
-        yield from self.storage.content_missing_per_sha1(contents)
-
-    def directory_missing(self, directories):
-        return self.storage.directory_missing(directories)
-
-    def revision_missing(self, revisions):
-        return self.storage.revision_missing(revisions)
-
-    def snapshot_get(self, snapshot_id):
-        return self.storage.snapshot_get(snapshot_id)
-
-    def content_get(self, content):
-        return self.storage.content_get(content)
+    def __getattr__(self, key):
+        return getattr(self.storage, key)
