cc342acede85e3b415b0568336a30103148c3694
Nicolas Dandrimont <nicolas@dandrimont.eu>
2020-09-25 17:08:51 +0200

    Drop vcversioner from setup.py (superseded by setuptools-scm)

--- setup.py
+++ setup.py
@@ -50,7 +50,6 @@ setup(
     setup_requires=["setuptools-scm"],
     use_scm_version=True,
     extras_require={"testing": parse_requirements("test")},
-    vcversioner={},
     include_package_data=True,
     entry_points="""
         [swh.workers]
