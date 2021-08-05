6118d43e6f55314cdd479ef45a9119dad0b8519a
Antoine R. Dumont (@ardumont) <ardumont@softwareheritage.org>
2021-08-05 16:07:59 +0200

    Revert "setup: Fix long description content type to rst"
    This reverts commit dbb18628f16d2cf68d8d71eb1c3cc6426017f3c4.
    
    This creates issues when uploading to pypi [1].
    
    The type x-rst is not posing problem for example in swh.core. But in that repository the
    readme is not a symlink... I'm not investigating this right now so a simple revert
    should do. My focus is on deploying the opam loader on staging for now.
    
    [1] https://jenkins.softwareheritage.org/view/swh-draft/job/DLDBASE/job/pypi-upload/111/console

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# Copyright (C) 2015-2021  The Software Heritage developers
+# Copyright (C) 2015-2018  The Software Heritage developers
 # See the AUTHORS file at the top-level directory of this distribution
 # License: GNU General Public License version 3, or any later version
 # See top-level LICENSE file for more information
@@ -39,7 +39,7 @@ setup(
     name="swh.loader.core",
     description="Software Heritage Base Loader",
     long_description=long_description,
-    long_description_content_type="text/x-rst",
+    long_description_content_type="text/markdown",
     python_requires=">=3.7",
     author="Software Heritage developers",
     author_email="swh-devel@inria.fr",
